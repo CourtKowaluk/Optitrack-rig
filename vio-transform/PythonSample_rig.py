49fcfb4f7453143da5920eacbae76d8a7a6a248b
Courtney Kowaluk <32277240+CourtKowaluk@users.noreply.github.com>
2020-02-12 19:16:43 -0500

    Add files via upload

--- vio-transform/PythonSample_rig.py
+++ vio-transform/PythonSample_rig.py
@@ -16,71 +16,103 @@ from NatNetClient import NatNetClient
 
 
 #The broker and port to connect to send MQTT messages.
-broker_url = "oz.andrew.cmu.edu"
+broker_url = 'oz.andrew.cmu.edu'
 broker_port = 1883
 
-name = "camera_pixel_pixel"
+name = 'camera_pixel_pixel'
 optitrack_id = 5
 
-viotopic = "/topic/vio/" + name
-rigtopic = "realm/s/earth/" + name
+viotopic = '/topic/vio/' + name
+rigtopic = 'realm/s/earth/' + name
 
 # Object pose according to two systems
 last_optitrack_time = 0
 last_optitrack_pose = np.identity(4)
-MAX_TIME_DELTA = 100000 # 100ms
-
+MAX_TIME_DELTA = 0.1 # 100ms
 
 # This is a callback function that gets connected to the NatNet client. It is called once per rigid body per frame
 def receiveRigidBodyFrame(id, position, rotation):
+    global last_optitrack_time
+    global last_optitrack_pose
+  
+    if(id==2): 
+        message = {
+        "object_id": "cube_t",
+        "action": "update",
+        "type": "object",
+        "data": {
+           "position": {
+                "x": position[0],
+                "y": position[1],
+                "z": position[2]
+            },
+            "rotation": {
+                "x": rotation[0],
+                "y": rotation[1],
+                "z": rotation[2],
+                "w": rotation[3]
+            },}}
+        client.publish("realm/s/earth/cube_t", json.dumps(message), qos=1, retain=False)
+        #print(json.dumps(message))
+
     if id == optitrack_id: #if the rigid body id (in Motive settings) is set to 5, so we don't get other rigid bodies we don't want
-        last_optitrack_pose[0:2, 0:2] = Rotation.from_quat(rotation).as_matrix()
-        last_optitrack_pose[0:2, 3] = position
-        last_optitrack_time = time.time()
+        new_optitrack_pose = np.identity(4)
+        new_optitrack_pose[0:3, 0:3] = Rotation.from_quat(rotation).as_matrix()
+        new_optitrack_pose[0:3, 3] = position
+        if (new_optitrack_pose != last_optitrack_pose).any():
+            last_optitrack_time = time.time()
+            last_optitrack_pose = new_optitrack_pose
 
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected With Result Code "+rc)
+    print('Connected With Result Code ' + rc)
 
 
 def on_message(client, userdata, message):
+    global last_optitrack_time
+    global last_optitrack_pose
     msg = message.payload.decode()
+    
     js = json.loads(msg)
+    
+    rot = js['data']['rotation']
+    rx = rot['x']
+    ry = rot['y']
+    rz = rot['z']
+    rw = rot['w']
 
-    rot = js["rotation"]
-    rx = rot["x"]
-    ry = rot["y"]
-    rz = rot["z"]
-    rw = rot["w"]
-
-    pos = js["position"]
-    px = pos["x"]
-    py = pos["y"]
-    pz = pos["z"]
+    pos = js['data']['position']
+    px = pos['x']
+    py = pos['y']
+    pz = pos['z']
 
     vio_pose = np.identity(4)
-    vio_pose[0:2, 0:2] = Rotation.from_quat([x, y, z, w]).as_matrix()
-    vio_pose[0:2, 3] = [px, py, pz]
+    vio_pose[0:3, 0:3] = Rotation.from_quat([rx, ry, rz, rw]).as_matrix()
+    vio_pose[0:3, 3] = [px, py, pz]
+    #print("vio pose: ")
+    #print(vio_pose)
 
     rig_pose = last_optitrack_pose.dot(np.linalg.inv(vio_pose))
-    rig_quat = Rotation.from_matrix(rig_pose[0:2, 0:2]).as_quat()
-    rig_pos = rig_pose[0:2, 3]
+    #print("rig pose: ")
+    #print(rig_pose)
+    rig_quat = Rotation.from_matrix(rig_pose[0:3, 0:3]).as_quat()
+    rig_pos = rig_pose[0:3, 3]
 
     rigmessage = {
-        "object_id": name,
-        "action": "update",
-        "type": "rig",
-        "data": {
-            "position": {
-                "x": rig_pos[0, 3],
-                "y": rig_pos[1, 3],
-                "z": rig_pos[2, 3]
+        'object_id': name,
+        'action': 'update',
+        'type': 'rig',
+        'data': {
+            'position': {
+                'x': rig_pos[0],
+                'y': rig_pos[1],
+                'z': rig_pos[2]
             },
-            "rotation": {
-                "x": rig_quat[0],
-                "y": rig_quat[1],
-                "z": rig_quat[2],
-                "w": rig_quat[3]
+            'rotation': {
+                'x': rig_quat[0],
+                'y': rig_quat[1],
+                'z': rig_quat[2],
+                'w': rig_quat[3]
             }
         }
     }
@@ -90,8 +122,9 @@ def on_message(client, userdata, message):
     tdiff = time.time() - last_optitrack_time
     if tdiff > 0 and tdiff < MAX_TIME_DELTA:
         client.publish(rigtopic, json.dumps(rigmessage), qos=1, retain=False)
+        print(tdiff)
     else:
-        print("optitrack too old (%f ms)", tdiff * 1000.0)
+        print('optitrack too old (%lf s)' % (tdiff))
 
 
 # This will create a new NatNet client
@@ -103,8 +136,10 @@ client.on_connect = on_connect
 client.on_message = on_message
 client.connect(broker_url, broker_port)
 client.subscribe(viotopic, qos=1)
+print(viotopic)
 
 # Configure the streaming client to call our rigid body handler on the emulator to send data out.
+streamingClient.newFrameListener = None
 streamingClient.rigidBodyListener = receiveRigidBodyFrame
 
 # Start up the streaming client now that the callbacks are set up.
